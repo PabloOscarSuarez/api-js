f5ddd0fbc88b4416e82ac08d068a5f065ac04c1a
polbac <polbac@gmail.com>
2022-02-02 20:51:14 -0300

    create task::label relation

--- controllers/task/createTask.js
+++ controllers/task/createTask.js
@@ -3,15 +3,17 @@ const badRequestError  = require('../../errors/badRequestError')
 
 async function createTask(req, res) {
     try {
-        const { description } = req.body
+        const { description, labelIds } = req.body
         
         const userId = req.user
 
         if (!description) {
             return res.status(400).send(badRequestError('Por favor ingrese una descripcion'))
         }
-
+        const labels = labelIds.split(",")
         const task = await createTaskDAO({ description, userId })
+        await task.addLabels(labels)
+        
         res.json(task)
     } catch(error) {
         console.log(error)
